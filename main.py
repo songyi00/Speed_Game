90d149b338f3d8115a8dc07511902b634ec1fe49
sonyi00 <ksl2950@naver.com>
2021-11-01 15:43:34 +0900

    feat : update gui

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from tkinter import *
+import tkinter.font as tkFont
 
 root = Tk()
 
 root.title("Game")
-root.geometry("300x200+100+100")
+root.geometry("600x300+100+100")
 root.resizable
 
+fontExample = tkFont.Font(family="Arial", size=40, weight="bold", slant="italic")
+# 레이블 생성
+label = Label(root, text="Speed Game")
+
+# 레이블을 화면에 배치
+label.pack()
+
+label.configure(font=fontExample)
 root.mainloop()
