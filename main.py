515e4b7b91a02c6144669d9d4f741acdf4cda295
leecr1215 <leecr1215@naver.com>
2021-11-01 16:01:37 +0900

    feat: change main.py line and variable

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ root.title("Game")
 root.geometry("600x300+100+100")
 root.resizable
 
-fontExample = tkFont.Font(family="Arial", size=40, weight="bold", slant="italic")
-# 레이블 생성
-label = Label(root, text="Speed Game")
+labelFont = tkFont.Font(family="Arial", size=40, weight="bold", slant="italic")
+startBtnFont = tkFont.Font()
 
+# 레이블 생성 + 옵션 추가
+label = Label(root, text="Speed Game")
+label.configure(font=labelFont)
+label.configure(background="white")
 # 레이블을 화면에 배치
 label.pack()
 
-# start 버튼 추가
-startBtn = Button(root, text='start')
-startBtn.pack()
+# start 버튼 추가 + 옵션 추가
+startBtn = Button(root, text="start")
+startBtn.configure(foreground="red")
+startBtn.configure(background="white")
+startBtn.configure(padx="50")
+startBtn.configure(font=startBtnFont)
+startBtn.pack()  # 화면에 배치
 
-label.configure(font=fontExample)
+root.configure(background="white")
 root.mainloop()
