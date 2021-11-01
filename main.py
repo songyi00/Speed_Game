4fa4532c5f7e3688d5651ef61c78fe0fc8e0c62b
leecr1215 <leecr1215@naver.com>
2021-11-01 16:21:30 +0900

    feat: change button color and padding

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import tkinter.font as tkFont
 
 root = Tk()
 
-root.title("Game")
-root.geometry("600x300+100+100")
+root.title("Speed Game")
+root.geometry("600x400+100+100")
 root.resizable
 
 labelFont = tkFont.Font(family="Arial", size=40, weight="bold", slant="italic")
-startBtnFont = tkFont.Font()
+startBtnFont = tkFont.Font(family="Consolas", size=20)
 
 # 레이블 생성 + 옵션 추가
 label = Label(root, text="Speed Game")
@@ -18,11 +18,13 @@ label.configure(background="white")
 label.pack()
 
 # start 버튼 추가 + 옵션 추가
-startBtn = Button(root, text="start")
-startBtn.configure(foreground="red")
-startBtn.configure(background="white")
-startBtn.configure(padx="50")
-startBtn.configure(font=startBtnFont)
+startBtn = Button(root, text="start")   # 버튼 text
+startBtn.configure(foreground="red")    # 버튼 문자열 색상
+startBtn.configure(background="white")  # 버튼 background 색상
+startBtn.configure(padx="100")          # 버튼 가로 padding
+startBtn.configure(font=startBtnFont)   # 버튼 폰트
+startBtn.configure(relief="ridge")
+
 startBtn.pack()  # 화면에 배치
 
 root.configure(background="white")
